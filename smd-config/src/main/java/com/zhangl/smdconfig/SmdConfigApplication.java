68e077c3827ccd05d8da42632b69ce356f67078d
rmzhang <2032941326@qq.com>
2018-08-15 16:22:23 +0800

    1.0

--- smd-config/src/main/java/com/zhangl/smdconfig/SmdConfigApplication.java
+++ smd-config/src/main/java/com/zhangl/smdconfig/SmdConfigApplication.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class SmdConfigApplication {
 
     private final static Logger logger = LoggerFactory.getLogger(SmdConfigApplication.class);
